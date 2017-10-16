d2a814d349dde7a7a58024e052666f163939a166
shaked <shaked@dataloop.ai>
2017-10-16 17:54:31 +0300

    added file

--- 2017_lecture_8_code_samples/torch_example_5.py
+++ 2017_lecture_8_code_samples/torch_example_5.py
@@ -1,24 +1,32 @@
 import torch
 from torch.autograd import Variable
 
+class TwoLayerNet(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        super(TwoLayerNet, self).__init__()
+        self.linear1 = torch.nn.Linear(D_in, H)
+        self.linear2 = torch.nn.Linear(H, D_out)
+
+    def forward(self, x):
+        h_relu = self.linear1(x).clamp(min=0)
+        y_pred = self.linear2(h_relu)
+        return y_pred
+
 N, D_in, H, D_out = 64, 1000, 100, 10
-x = Variable(torch.randn(N, D_in), requires_grad=False)
+x = Variable(torch.randn(N, D_in),)
 y = Variable(torch.randn(N, D_out), requires_grad=False)
 
-model = torch.nn.Sequential(
-    torch.nn.Linear(D_in, H),
-    torch.nn.ReLU(),
-    torch.nn.Linear(H, D_out),
-)
+model = TwoLayerNet(D_in, H, D_out)
+
 loss_fn = torch.nn.MSELoss(size_average=False)
 
-learning_rate = 1e-4
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+criterion = torch.nn.MSELoss(size_average=False)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 for t in range(50):
     y_pred = model(x)
-    loss = loss_fn(y_pred, y)
+    loss = criterion(y_pred, y)
+    
     print(loss.data.numpy()[0])
-    model.zero_grad()
+    optimizer.zero_grad()
     loss.backward()
-
     optimizer.step()
